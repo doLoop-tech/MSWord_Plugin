e043c0b5a78412557050cef6c1b08dc0b105daeb
Manav <manavdesai30399@gmail.com>
2020-06-20 13:14:23 +0530

    small changes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+import "zone.js"; // Required for Angular
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import {AppModule} from "./app/app.module";
+/* global console, document, Office */
 
 Office.initialize = () => {
   document.getElementById("sideload-msg").style.display = "none";
@@ -16,4 +15,3 @@ Office.initialize = () => {
     .bootstrapModule(AppModule)
     .catch(error => console.error(error));
 };
-
