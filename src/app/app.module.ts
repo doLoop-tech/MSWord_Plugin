291dd642b3138a7d58a01507cb14de7c54049c43
Manav <manavdesai30399@gmail.com>
2020-06-20 18:51:13 +0530

    major change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {APP_BASE_HREF} from '@angular/common';
+import{HttpClientModule} from '@angular/common/http';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoundPipe } from './pipes/round.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoundPipe
   ],
   imports: [
     BrowserModule,
+    HttpClientModule
   ],
   providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent]
