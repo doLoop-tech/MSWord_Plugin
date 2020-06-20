02dead6f37bc02ec7bdf0c6ff32bbdbbc7fed2f4
Manav <manavdesai30399@gmail.com>
2020-06-20 12:08:07 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+Office.initialize = () => {
+  document.getElementById("sideload-msg").style.display = "none";
+
+  // Bootstrap the app
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch(error => console.error(error));
+};
+
