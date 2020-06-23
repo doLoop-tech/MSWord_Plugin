ba7d182778501aa8233479756616bbf640d67352
Manav <manavdesai30399@gmail.com>
2020-06-23 19:21:43 +0530

    login and profile feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone, ɵɵcontainerRefreshEnd } from '@angular/core';
 import { ContentService } from 'src/app/services/content.service';
 import { DiffMatchPatch } from 'ng-diff-match-patch';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -22,45 +23,81 @@ export class AppComponent implements OnInit{
   public textDiff;
   public showDiff :Boolean;
   public selectedText;
+  public showLogin : Boolean;
+  public loginForm  :FormGroup;
+  public token  : any;
+  public failure  :Boolean;
+  public error;
+  public user;
+  public name;
 
-  constructor(private content : ContentService , private zone : NgZone){}
+  constructor(private content : ContentService , private fb : FormBuilder ,  private zone : NgZone){
+    this.loginForm = this.fb.group({
+      "username" : [''],
+      "password" : ['']
+    })
+  }
   ngOnInit(){
-    this.dmp = new DiffMatchPatch();
-    document.getElementById("text").innerHTML = "<p></p>"
+    if(localStorage.getItem("token")){
+      this.showSimilarity = false;
+      this.dmp = new DiffMatchPatch();
+      document.getElementById("text").innerHTML = "<p></p>"
+      this.content.getProfile().subscribe((data)=>{
+        this.zone.run(()=>{
+          this.user = data;
+          this.name = this.user.data.firstName + " "+this.user
+          .data.lastName
+        })
+      })
+    }else{
+      this.showLogin = true;
+
+    }
+    
 
   }
   
 
    run() {
-     this.showSimilarity = true;
-     this.showDiff = false;
-     document.getElementById("text").innerHTML = "<p></p>"
-     Word.run((context)=>{
-      Office.context.document.getSelectedDataAsync(Office.CoercionType.Text , (result)=>{
-        this.text = result.value;
-        this.content.getContent(this.text).subscribe((data)=>{
-          this.zone.run(()=>{
-            this.data = data.data;
-            this.sourceContent = data.data.similar_text.source_content;
-            this.approvedContent = data.data.similar_text.approved_content;
-            this.list = this.sourceContent.concat(this.approvedContent)
-            
-          });
-        });
+     if(localStorage.getItem("token")){
+      this.showSimilarity = true;
+      this.showDiff = false;
+      document.getElementById("text").innerHTML = "<p></p>"
+      Word.run((context)=>{
+       Office.context.document.getSelectedDataAsync(Office.CoercionType.Text , (result)=>{
+         this.text = result.value;
+         this.content.getContent(this.text).subscribe((data)=>{
+           this.zone.run(()=>{
+             this.data = data.data;
+             this.sourceContent = data.data.similar_text.source_content;
+             this.approvedContent = data.data.similar_text.approved_content;
+             this.list = this.sourceContent.concat(this.approvedContent)
+             
+           });
+         });
+       });
+       return context.sync()
       });
-      return context.sync()
-     });
+     }else{
+       this.reset();
+     }
+     
   }
 
   show(text){
-   this.showDiff = true;
-   this.selectedText = text;
-    this.showSimilarity = false;
-   //this.textDiff = text;
-   this.textDiff = this.dmp.diff_main(this.data.input_text , text);
-   this.dmp.diff_cleanupSemantic(this.textDiff);
-   this.original = this.dmp.diff_prettyHtml(this.textDiff);
-   document.getElementById("text").innerHTML = this.original;
+    if(localStorage.getItem("token")){
+      this.showDiff = true;
+      this.selectedText = text;
+       this.showSimilarity = false;
+      //this.textDiff = text;
+      this.textDiff = this.dmp.diff_main(this.data.input_text , text);
+      this.dmp.diff_cleanupSemantic(this.textDiff);
+      this.original = this.dmp.diff_prettyHtml(this.textDiff);
+      document.getElementById("text").innerHTML = this.original;
+    }else{
+     this.reset();
+    }
+   
 
   }
 
@@ -70,6 +107,47 @@ export class AppComponent implements OnInit{
      this.showSimilarity = true
 
   }
+
+  login(){
+    const username = this.loginForm.get("username").value;
+    const password = this.loginForm.get("password").value;
+    this.content.login(username , password).subscribe((data)=>{
+      this.zone.run(()=>{
+        this.token = data;
+        if(this.token.error!=undefined ||this.token.error!=null ){
+          this.failure = true;
+          this.error = this.token.error;
+        }else{
+          this.failure = false;
+          localStorage.setItem("token" , this.token.token);
+          this.showLogin = false;
+          this.showSimilarity = true;
+          this.content.getProfile().subscribe((data)=>{
+            this.zone.run(()=>{
+              this.user = data;
+              this.name = this.user.data.firstName + " "+this.user
+              .data.lastName
+            })
+          })
+        }
+        
+      })
+    })
+
+  }
+
+  logout(){
+    localStorage.removeItem("token");
+    this.reset();
+  }
   
+  reset(){
+    this.loginForm.get("username").setValue("")
+       this.loginForm.get("password").setValue("");
+       this.showLogin = true;
+       this.showDiff = false;
+       this.showSimilarity = false;
+  }
+ 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import {APP_BASE_HREF} from '@angular/common';
 import{HttpClientModule} from '@angular/common/http';
 import{DiffMatchPatchModule} from 'ng-diff-match-patch';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 
 
@@ -19,7 +21,9 @@ import { RoundPipe } from './pipes/round.pipe';
   imports: [
     BrowserModule,
     HttpClientModule,
-    DiffMatchPatchModule
+    DiffMatchPatchModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent]
